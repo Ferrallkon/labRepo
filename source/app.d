16765294fdab74aeb12a0fdfc808b2acbc3b45a2
MJ <sungwonkim20@gmail.com>
2026-09-21 17:19:54 -0600

    added 'restart', ball collision logic, ball.update, ball.drawball, and small edits

--- source/app.d
+++ source/app.d
@@ -3,16 +3,18 @@ import std.conv;
 import raylib;
 
 import packages.players;
+import core.sys.windows.wingdi;
+import core.sys.linux.input_event_codes;
 
 void main()
 {
 
 	auto p1 = Player(Vector2(10, 10), Vector2(100, 50), Colors.RED);
-	auto p2 = Player(Vector2(700, 700), Vector2(100, 50), Colors.BLUE);
+	auto p2 = Player(Vector2(690, 740), Vector2(100, 50), Colors.BLUE);
 	auto ball = Ball();
 
 	validateRaylibBinding();
-	InitWindow(800, 800, "windowName");
+	InitWindow(800, 800, "Game");
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
@@ -26,25 +28,41 @@ void main()
 		DrawRectangle(p2.position.x.to!int, p2.position.y.to!int, p2.size.x.to!int, p2.size.y.to!int, p2
 				.color);
 
-		if (IsKeyDown(KeyboardKey.KEY_RIGHT))
-			p1.position.x += 2.0f;
-		if (IsKeyDown(KeyboardKey.KEY_LEFT))
-			p1.position.x -= 2.0f;
-		if (IsKeyDown(KeyboardKey.KEY_UP))
-			p1.position.y -= 2.0f;
-		if (IsKeyDown(KeyboardKey.KEY_DOWN))
-			p1.position.y += 2.0f;
+        ball.drawBall(); // ball method
+
+        if(IsKeyPressed(KeyboardKey.KEY_R)){ // press 'R' for quick reset, just for testing purposes
+            p1.position = Vector2(10, 10);
+            p2.position = Vector2(690, 740);
+            ball.position = Vector2(400.0f, 400.0f);
+            ball.speed = Vector2(0.0f, 0.0f);
+        }
 
 		if (IsKeyDown(KeyboardKey.KEY_D))
-			p2.position.x += 2.0f;
+			p1.position.x += 2.0f;
 		if (IsKeyDown(KeyboardKey.KEY_A))
-			p2.position.x -= 2.0f;
+			p1.position.x -= 2.0f;
 		if (IsKeyDown(KeyboardKey.KEY_W))
-			p2.position.y -= 2.0f;
+			p1.position.y -= 2.0f;
 		if (IsKeyDown(KeyboardKey.KEY_S))
+			p1.position.y += 2.0f;
+
+		if (IsKeyDown(KeyboardKey.KEY_RIGHT))
+			p2.position.x += 2.0f;
+		if (IsKeyDown(KeyboardKey.KEY_LEFT))
+			p2.position.x -= 2.0f;
+		if (IsKeyDown(KeyboardKey.KEY_UP))
+			p2.position.y -= 2.0f;
+		if (IsKeyDown(KeyboardKey.KEY_DOWN))
 			p2.position.y += 2.0f;
-		// DrawCircle(int centerX, int centerY, float radius, Color color);      
-		DrawCircle(ball.position.x.to!int, ball.position.y.to!int, ball.radius, Colors.GRAY);
+
+        // convert players to Rectangle to use CheckCollisionCircleRec()
+        auto p1rec = Rectangle(p1.position.x, p1.position.y, p1.size.x, p1.size.y);
+        auto p2rec = Rectangle(p2.position.x, p2.position.y, p2.size.x, p2.size.y);
+
+        ball.checkCollisionWithPlayer(p1rec); // ball method
+        ball.checkCollisionWithPlayer(p2rec);
+        ball.update(); // ball method
+        ball.drawBall();
 
 		EndDrawing();
 	}
--- source/packages/players.d
+++ source/packages/players.d
@@ -1,5 +1,6 @@
 module packages.players;
 import raylib;
+import std.conv;
 
 struct Player
 {
@@ -8,14 +9,40 @@ struct Player
     Color color;
 }
 
+
+
 struct Ball
 {
-    Vector2 position = {500.0f, 500.0f};
+    Vector2 position = {400.0f, 400.0f};
     Vector2 speed = {0.0f, 0.0f};
-    float radius = 20;
+    float radius = 25;
+    Color color = Colors.GRAY;
 
-    // void moveBall(){
+    void drawBall(){
+        DrawCircle(position.x.to!int, position.y.to!int, radius, color);
+    }
 
-    // }
+    void update(){
+        position.x += speed.x;
+        position.y += speed.y;
+    }
+
+    void checkCollisionWithPlayer(Rectangle player){
+        if(CheckCollisionCircleRec(position, radius, player)){
+            float playerCenterX = player.x + (player.width/2.0f); // center of players x axis
+            float playerCenterY = player.y + (player.height/2.0f); // center of players y axis
+
+            if(position.x > playerCenterX){ // if ball is to the right of center
+                speed.x = 1.0f; // ball move right
+            } else { // if ball is to the left
+                speed.x = -1.0f; // ball move left
+            }
+            if(position.y > playerCenterY){ // if ball is below center 
+                speed.y = 1.0f; // ball move down because y axis top left is 0
+            } else { // if ball is above center 
+                speed.y = -1.0f; // ball move up
+            }
+        }
+    }
 
 }
