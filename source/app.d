50542f393407813e6f250285566b3bf0c7ff3075
Cristian Sanchez Velasco <cristiankaipi@outlook.com>
2026-09-21 12:17:34 -0600

    window

--- source/app.d
+++ source/app.d
@@ -1,6 +1,17 @@
 import std.stdio;
+import raylib;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	validateRaylibBinding();
+	InitWindow(800, 800, "windowName");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(Colors.RAYWHITE);
+		EndDrawing();
+	}
+	CloseWindow();
 }
