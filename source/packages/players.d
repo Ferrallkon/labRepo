eb5be522c9dfd75302aa4ac987ad6d52d08c8e03
MJ <sungwonkim20@gmail.com>
2026-09-21 11:59:30 -0600

    commit1

---
+++ source/packages/players.d
@@ -0,0 +1,20 @@
+module packages.players;
+
+struct Player {
+    char direction; // left right up down l r u d
+    bool ballContact; // touching a ball
+    float speed; // speed of movement
+
+
+
+}
+
+
+
+struct Ball {
+
+
+
+
+
+}
