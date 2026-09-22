53038e03b26c7dcbb8f5e7d336882c1ac109049f
Cristian Sanchez Velasco <cristiankaipi@outlook.com>
2026-09-22 10:20:12 -0600

    changed player to circle

--- source/app.d
+++ source/app.d
@@ -9,8 +9,8 @@ import core.sys.linux.input_event_codes;
 void main()
 {
 
-	auto p1 = Player(Vector2(10, 10), Vector2(100, 50), Colors.RED);
-	auto p2 = Player(Vector2(690, 740), Vector2(100, 50), Colors.BLUE);
+	auto p1 = Player(Vector2(10, 10), Colors.RED);
+	auto p2 = Player(Vector2(690, 740), Colors.BLUE);
 	auto ball = Ball();
 
 	validateRaylibBinding();
@@ -22,20 +22,18 @@ void main()
 		BeginDrawing();
 		ClearBackground(Colors.RAYWHITE);
 
-		DrawRectangle(p1.position.x.to!int, p1.position.y.to!int, p1.size.x.to!int, p1.size.y.to!int, p1
-				.color);
+		p1.drawPlayer();
+		p2.drawPlayer();
 
-		DrawRectangle(p2.position.x.to!int, p2.position.y.to!int, p2.size.x.to!int, p2.size.y.to!int, p2
-				.color);
+		ball.drawBall(); // ball method
 
-        ball.drawBall(); // ball method
-
-        if(IsKeyPressed(KeyboardKey.KEY_R)){ // press 'R' for quick reset, just for testing purposes
-            p1.position = Vector2(10, 10);
-            p2.position = Vector2(690, 740);
-            ball.position = Vector2(400.0f, 400.0f);
-            ball.speed = Vector2(0.0f, 0.0f);
-        }
+		if (IsKeyPressed(KeyboardKey.KEY_R))
+		{ // press 'R' for quick reset, just for testing purposes
+			p1.position = Vector2(10, 10);
+			p2.position = Vector2(690, 740);
+			ball.position = Vector2(400.0f, 400.0f);
+			ball.speed = Vector2(0.0f, 0.0f);
+		}
 
 		if (IsKeyDown(KeyboardKey.KEY_D))
 			p1.position.x += 2.0f;
@@ -55,14 +53,14 @@ void main()
 		if (IsKeyDown(KeyboardKey.KEY_DOWN))
 			p2.position.y += 2.0f;
 
-        // convert players to Rectangle to use CheckCollisionCircleRec()
-        auto p1rec = Rectangle(p1.position.x, p1.position.y, p1.size.x, p1.size.y);
-        auto p2rec = Rectangle(p2.position.x, p2.position.y, p2.size.x, p2.size.y);
+		// // convert players to Rectangle to use CheckCollisionCircleRec()
+		auto p1rec = Rectangle(p1.position.x, p1.position.y, p1.size.x, p1.size.y);
+		auto p2rec = Rectangle(p2.position.x, p2.position.y, p2.size.x, p2.size.y);
 
-        ball.checkCollisionWithPlayer(p1rec); // ball method
-        ball.checkCollisionWithPlayer(p2rec);
-        ball.update(); // ball method
-        ball.drawBall();
+		ball.checkCollisionWithPlayer(p1rec); // ball method
+		ball.checkCollisionWithPlayer(p2rec);
+		ball.update(); // ball method
+		ball.drawBall();
 
 		EndDrawing();
 	}
--- source/packages/players.d
+++ source/packages/players.d
@@ -4,12 +4,18 @@ import std.conv;
 
 struct Player
 {
+
     Vector2 position;
-    Vector2 size;
     Color color;
+
+    float radius = 50;
+
+    void drawPlayer()
+    {
+        DrawCircle(position.x.to!int, position.y.to!int, radius, color);
+    }
+
 }
-
-
 
 struct Ball
 {
@@ -18,28 +24,38 @@ struct Ball
     float radius = 25;
     Color color = Colors.GRAY;
 
-    void drawBall(){
+    void drawBall()
+    {
         DrawCircle(position.x.to!int, position.y.to!int, radius, color);
     }
 
-    void update(){
+    void update()
+    {
         position.x += speed.x;
         position.y += speed.y;
     }
 
-    void checkCollisionWithPlayer(Rectangle player){
-        if(CheckCollisionCircleRec(position, radius, player)){
-            float playerCenterX = player.x + (player.width/2.0f); // center of players x axis
-            float playerCenterY = player.y + (player.height/2.0f); // center of players y axis
+    void checkCollisionWithPlayer(Rectangle player)
+    {
+        if (CheckCollisionCircleRec(position, radius, player))
+        {
+            float playerCenterX = player.x + (player.width / 2.0f); // center of players x axis
+            float playerCenterY = player.y + (player.height / 2.0f); // center of players y axis
 
-            if(position.x > playerCenterX){ // if ball is to the right of center
+            if (position.x > playerCenterX)
+            { // if ball is to the right of center
                 speed.x = 1.0f; // ball move right
-            } else { // if ball is to the left
+            }
+            else
+            { // if ball is to the left
                 speed.x = -1.0f; // ball move left
             }
-            if(position.y > playerCenterY){ // if ball is below center 
+            if (position.y > playerCenterY)
+            { // if ball is below center 
                 speed.y = 1.0f; // ball move down because y axis top left is 0
-            } else { // if ball is above center 
+            }
+            else
+            { // if ball is above center 
                 speed.y = -1.0f; // ball move up
             }
         }
